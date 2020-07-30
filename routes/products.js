ff52585c8057b32cbcf912fc0f83aca5ccbf6ae3
Pedro Ferraz de Andrade <pedro.andrade@meucheckin.com>
2020-07-29 21:16:50 -0300

    inicio projeto

---
+++ routes/products.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const productsController = require('./productsController');
+const router = express.Router();
+
+/* GET product page. */
+router.get('/', productsController.get);
+router.get('/:id', productsController.getById);
+router.post('/',productsController.create);
+module.exports = router;
